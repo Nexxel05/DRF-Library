d363712895f2be025917afdf4f865df72bdbc450
Kostyantyn Chernyk <chernykko@gmail.com>
2023-03-06 19:45:28 +0000

    Add BorrowingCreateSerializer and BorrowingListSerializer

--- borrowings_service/serializers.py
+++ borrowings_service/serializers.py
@@ -7,15 +7,39 @@ from borrowings_service.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    book = BookListDetailSerializer(read_only=True)
-    borrow_date = serializers.DateField(initial=datetime.datetime.today())
 
     class Meta:
         model = Borrowing
         fields = (
+            "id",
             "borrow_date",
             "expected_return_date",
             "actual_return_date",
             "book",
             "customer"
         )
+
+
+class BorrowingCreateSerializer(BorrowingSerializer):
+    class Meta:
+        model = Borrowing
+        exclude = ("customer",)
+
+    def create(self, validated_data):
+        borrowing = Borrowing.objects.create(**validated_data)
+        book = borrowing.book
+        book.inventory -= 1
+        book.save()
+        return borrowing
+
+    def validate(self, attrs):
+        data = super(BorrowingCreateSerializer, self).validate(attrs)
+        if data["book"].inventory == 0:
+            raise serializers.ValidationError(
+                f"The {data['book'].title} is out of stock"
+            )
+        return data
+
+
+class BorrowingListSerializer(BorrowingSerializer):
+    book = BookListDetailSerializer(read_only=True)
