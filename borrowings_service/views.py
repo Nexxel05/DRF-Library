b10dff2af88c0451fa87ba647749c3a47fd60488
Kostyantyn Chernyk <chernykko@gmail.com>
2023-03-06 21:04:17 +0000

    Configure filtering by user_id or/and is_active parameters and add authentication

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
 from borrowings_service.models import Borrowing
 from borrowings_service.serializers import (
@@ -14,9 +15,25 @@ class BorrowingView(
     mixins.CreateModelMixin
 ):
     serializer_class = BorrowingListSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         queryset = Borrowing.objects.all()
+
+        is_active = self.request.query_params.get("is_active")
+
+        if is_active:
+            queryset = queryset.filter(actual_return_date__isnull=True)
+
+        if self.request.user.is_staff:
+
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(customer__id=user_id)
+
+            return queryset
+
+        queryset = queryset.filter(customer=self.request.user)
 
         return queryset
 
