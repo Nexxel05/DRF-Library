5a9df7c1192df4084859d3df20d9b66fe69e0d81
Kostyantyn Chernyk <chernykko@gmail.com>
2023-03-31 12:24:34 +0100

    Create home template and configure it

--- drf_library/urls.py
+++ drf_library/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("books/", include(
         "books_service.urls", namespace="books_service"
     )),
